59425b83c0450b1025336809a6aeacedf1a1b3ba
annuu1 <mannusng876@gmail.com>
2025-08-21 11:59:47 +0530

    feat: add the pages for testing purpose

---
+++ src/models/User.ts
@@ -0,0 +1,13 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+);
+
+const User = models.User || model("User", userSchema);
+
+export default User;
